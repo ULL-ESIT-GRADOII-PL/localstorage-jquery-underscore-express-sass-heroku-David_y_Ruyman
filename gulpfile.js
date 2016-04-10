ec8d5c6ea90bd851e60a65cbb0773db19d690c85
alu0100818208 <alu0100818208@ull.edu.es>
2016-04-10 18:21:03 +0100

    Modificado gulp basico.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,5 @@
- var gulp    = require('gulp'),
- gutil   = require('gulp-util');
- var ghPages = require('gulp-gh-pages');
+var gulp = require('gulp');
 
- gulp.task('deploy', function() {
-  return gulp.src(['./*.css', './*.txt', './*.md', './*.js', './*.html'])
-     .pipe(ghPages());
-  });
+gulp.task('default', function() {
+ // place code for your default task here
+});
