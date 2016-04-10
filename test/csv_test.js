7f3e64d820a0d153a7f79579f341a59a81348b7b
alu0100818208 <alu0100818208@ull.edu.es>
2016-04-10 18:12:01 +0100

    Añadidas nuevas pruebas.

--- test/csv_test.js
+++ test/csv_test.js
@@ -15,22 +15,41 @@ var expect = chai.expect;
    });
 
    describe("Entrada.", function() {
+     //----------Una sola columna---------------
      it("Debería poderse introducir una cadena y que sea aceptada.", function() {
        var cadena = '"Prueba"';
        var r = calculate(cadena);
        expect(r[0].value[0]).to.equal('Prueba');
      });
 
-     it("Prueba con comas a los lados", function() {
+     it("Comas a los lados", function() {
        var cadena = '",Prueba,"';
        var r = calculate(cadena);
        expect(r[0].value[0]).to.equal(',Prueba,');
      });
 
-     it("Prueba con espacios a los lados", function() {
-       var cadena = '" hola "';
+     it("Espacios a los lados", function() {
+       var cadena = '" PruebaA "';
        var r = calculate(cadena);
-       expect(r[0].value[0]).to.equal(' hola ');
+       expect(r[0].value[0]).to.equal(' PruebaA ');
      });
+
+     //-----------Dos columnas-----------------
+     it("Separando con comas", function() {
+       var cadena = '"PruebaA","PruebaB"';
+       var r = calculate(cadena);
+       expect(r[0].value[0]).to.equal('PruebaA');
+       expect(r[0].value[1]).to.equal('PruebaB');
+     });
+
+     //---------Cuatro columnas-------------------
+     it("Espacio en balnco con dos comas seguidas.", function() {
+       var cadena = ' " ","PruebaA",,"PruebaB"';
+       var r = calculate(cadena);
+       expect(r[0].value[0]).to.equal(' ');
+       expect(r[0].value[1]).to.equal('PruebaA');
+       expect(r[0].value[2]).to.equal('');
+       expect(r[0].value[3]).to.equal('PruebaB');
+     })
    });
  });
