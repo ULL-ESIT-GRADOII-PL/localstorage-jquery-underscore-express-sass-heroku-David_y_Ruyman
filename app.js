107dbbd19741a3d72fbd37b34f0eb7337530c0ed
alu0100845235 <alu0100845235@ull.edu.es>
2016-04-11 08:57:02 +0100

    Cambiando puerto de acceso

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use (express.static ('public'));
 app.use(expressLayouts);
 
 // Luego la consultamos con app.get('port')
-app.set('port', (process.env.PORT || 8080)); 
+app.set('port', (process.env.PORT || 5000)); 
 
 var parser = require('./public/js/csv.js')
 
